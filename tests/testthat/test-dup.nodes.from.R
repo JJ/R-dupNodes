a82d5a0f85e15d16882d989a2995f5238ba8fec1
JJ Merelo <jjmerelo@gmail.com>
2024-01-31 09:50:55 +0100

    :white_check_mark:

--- tests/testthat/test-dup.nodes.from.R
+++ tests/testthat/test-dup.nodes.from.R
@@ -18,7 +18,7 @@ colnames(df) <- c("node1", "node2")
 test_that("Graph with custom values", {
   dup.graph <- dup.nodes.from.data.frame(df, first.node="node1", second.node="node2")
   expect_equal( length(V(dup.graph)), length(different.nodes)+1 )
-  print(incident(dup.graph,"A'"))
+  expect_equal( length(E(dup.graph)[ "A" %--% "A'"] ),1)
   expect_equal( length(incident(dup.graph,"A")), length(incident(dup.graph,"A'")) )
 })
 
