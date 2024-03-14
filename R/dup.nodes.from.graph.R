4aa8fb453bd80a053ac3950171aa43ea66b1381e
JJ Merelo <jjmerelo@gmail.com>
2024-03-14 14:43:58 +0100

    #11 finished duplication

--- R/dup.nodes.from.graph.R
+++ R/dup.nodes.from.graph.R
@@ -12,11 +12,35 @@ dup.nodes.from.graph <- function( graph.with.self.loops ) {
     return( graph.with.self.loops )
   }
   modified.graph <- graph.with.self.loops
-  self.edges <- as_edgelist(modified.graph)[ which_loop(modified.graph),]
+  original.edges <- as_edgelist(modified.graph)
+  self.edges <- original.edges[ which_loop(modified.graph),]
 
   nodes.with.self.loops <- unique(self.edges[,1])
+  # First add nodes
   for ( node in nodes.with.self.loops ) {
-    modified.graph <- add_vertices(modified.graph,1,name=paste0(node,"'"))
+    new.node <- paste0(node,"'")
+    modified.graph <- add_vertices(modified.graph,1,name=new.node)
+  }
+
+  # Then add edges to nodes
+  for ( node in nodes.with.self.loops ) {
+    new.node <- paste0(node,"'")
+    for ( vertex in original.edges[ original.edges[,1] == node,2] ) {
+      if ( vertex != node ) {
+        modified.graph <- add_edges(modified.graph, c(vertex,new.node))
+        if ( vertex %in% nodes.with.self.loops ) {
+          modified.graph <- add_edges(modified.graph, c(paste0(vertex,"'"),new.node))
+        }
+      }
+    }
+    for ( vertex in original.edges[ original.edges[,2] == node,1] ) {
+      if ( vertex != node ) {
+        modified.graph <- add_edges(modified.graph, c(new.node,vertex))
+        if ( vertex %in% nodes.with.self.loops ) {
+          modified.graph <- add_edges(modified.graph, c(new.node,paste0(vertex,"'")))
+        }
+      }
+    }
   }
 
   for ( node in  self.edges[,1]) {
