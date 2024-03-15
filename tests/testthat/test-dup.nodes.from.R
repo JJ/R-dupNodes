b4b2f976f62c070e7396ab0d9a918283333ff298
JJ Merelo <jjmerelo@gmail.com>
2024-03-15 17:12:32 +0100

    Add another test, just in case

--- tests/testthat/test-dup.nodes.from.R
+++ tests/testthat/test-dup.nodes.from.R
@@ -1,6 +1,16 @@
 
 library(igraph)
 
+V1 <- c("A", "A","B")
+V2 <- c("A", "B", "B")
+df <- data.frame(V1, V2)
+
+test_that("simple graphs works", {
+  dup.graph <- dup.nodes.from.data.frame(df)
+  expect_equal( length(V(dup.graph)), 3 )
+  expect_equal( length(E(dup.graph)), 6 )
+})
+
 V1 <- c("A", "A","B","C","D","E")
 V2 <- c("A", "B","C","D","E","A")
 
